4fd197414e1d39bde34c583e2530bd4eb834effa
ian-mendez07 <mendezj8632@student.faytechcc.edu>
2026-01-27 03:18:15 +0000

    finished m1t1

--- m1t1_demo.cpp
+++ m1t1_demo.cpp
@@ -10,5 +10,12 @@ ianmendez
 using namespace std;
   
 int main(){
+  // greet the suer
+  cout << "Hello, user!" << endl; // this is "endline"
+  cout << "What is your name?" << endl;
+  string name; // variable to hold name
+  cin >> name; // get user input
+  cout << "Nice to meet you, " << name << endl;
+  
     return 0;
 }
