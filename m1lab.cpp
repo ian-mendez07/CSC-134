6e8fd53f8ff755c122dcf59de7cc4c73cc24b556
ian-mendez07 <mendezj8632@student.faytechcc.edu>
2026-02-02 19:56:29 +0000

    m1lab_test

---
+++ m1lab.cpp
@@ -0,0 +1,25 @@
+// CSC 134
+// M1LAB
+// Jeovannie Mendez
+// 02/02/2026
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // This program will simulate an apple orchard.
+    // The owners name
+    string name = "Your Name"
+    // # of apples owned
+    int apples = 100
+    // price per apple
+    double pricePerApple = 0.25
+
+    // calculate the total price of the apples
+    // TODO
+
+    // print all the information about the orchard
+    // TODO
+    cout << "Welcome to " << name;
+    cout << " 's apple orchard." << endl;
+}
