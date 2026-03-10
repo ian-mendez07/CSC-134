343b8fe58ee165b008af42ba69fc9f5bfabddf7c
ian-mendez07 <mendezj8632@student.faytechcc.edu>
2026-03-10 00:24:34 +0000

    question 1 final

---
+++ m3hw1.cpp
@@ -0,0 +1,34 @@
+// CSC 134
+// M3HW1 - Gold
+// Jeovannie Mendez
+// 03/09/2026
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Question 1
+    string answer_1;
+    cout << "Hello! How's your day?" << endl;
+    cin >> answer_1;
+    if (answer_1 == "Good"){
+        cout << "That is good to hear!" << endl;
+    }
+    else if (answer_1 == "Bad"){
+        cout << "Perhaps tomorrow will be good!" << endl;
+    }
+    else {
+        cout << "Well... I hope it gets better :P" << endl;
+    }
+
+
+    // Question 2
+    cout << "Question 2" << endl;
+    // code here
+
+    // Question 3
+    cout << "Question 3" << endl;
+    // code here
+
+    return 0;
+}
