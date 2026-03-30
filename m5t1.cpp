c1f5157dbcdb876c27f30754e073a9085ccea610
ian-mendez07 <mendezj8632@student.faytechcc.edu>
2026-03-30 04:26:20 +0000

    m5t1 ?uhh

---
+++ m5t1.cpp
@@ -0,0 +1,43 @@
+/*
+CSC 134
+M5T1 - Intro to functions
+Mendez
+3/29/2026
+*/
+
+#include <iostream>
+using namespace std;
+
+/*
+Goal: to create one void function
+one value returning function
+*/
+
+void say_hello();
+int give_the_answer();
+int double_a_number();
+
+//Function defintions
+int main(){
+    cout << "Hello from main()" << endl;
+    say_hello();
+    int my_answer = give_the_answer();
+    cout << "The answer is: " << my_answer << endl;
+    int twotimes = double_a_number(7);
+    cout << "Here's another number: " << twotimes<< endl;
+    return 0;
+}
+
+void say_hello(){
+    cout << "Hi from say_hello() " << endl;
+    return;
+}
+
+int give_the_answer(){
+    return 42;
+}
+
+int double_a_number(int num){
+    int new_num = num * 2;
+    return new_num;
+}
