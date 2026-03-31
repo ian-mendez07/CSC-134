91e2825123d7fddb339e680c79e2ca3892e91a1a
ian-mendez07 <mendezj8632@student.faytechcc.edu>
2026-03-31 03:26:37 +0000

    m5t2 test

---
+++ m5t2.cpp
@@ -0,0 +1,25 @@
+/*
+CSC 134
+M5T2 - More Practice with Functions
+Mendez
+3/30/2026
+*/
+#include <iostream> 
+using namespace std;
+
+//given two numbers and prints them out
+void printResult(int first, int second){
+    cout << first << " ^2 = " << second << endl;
+}
+
+int main(){
+    int count = 1;
+    int result;
+
+    while (count <= 10){
+        result = count * count;
+        //cout << count << " ^2 = " << result << endl;
+        printResult(count, result);
+        count++;
+    }
+}
