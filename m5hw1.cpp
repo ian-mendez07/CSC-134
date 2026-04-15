29fb0a625ec379c77e626d2366b104f021c4e488
ian-mendez07 <mendezj8632@student.faytechcc.edu>
2026-04-15 20:53:45 +0000

    m5hw1 1.1

--- m5hw1.cpp
+++ m5hw1.cpp
@@ -10,6 +10,51 @@ Mendez
 #include <cmath> //calls rand
 using namespace std;
 
+void question_1();
+void question_2();
+void question_3();
+void question_4();
+void question_5();
+
+
+int main(){
+    int question_choice = 0;
+    while (question_choice !=6){
+        cout << "1. Rainfall Average"<< endl;
+        cout << "2. Block Volume"<< endl;
+        cout << "3. Roman numerals"<< endl;
+        cout << "4. Geometry Calculator"<< endl;
+        cout << "5. Distance Traveled"<< endl;
+        cout << "6. Quit"<< endl;
+        cout << "Enter 1-6 for type of calculator: "<< endl;
+        cin>> question_choice;
+
+        if (question_choice==1){
+            question_1();
+        }
+        else if (question_choice==2){
+            question_2();
+        }
+        else if (question_choice==3){
+            question_3();
+        }
+        else if (question_choice==4){
+            question_4();
+        }
+        else if (question_choice==5){
+            question_5();
+        }
+        else if (question_choice==6){
+            cout << "Goodbye"<< endl;
+        }
+        else{
+            cout << "Choices are 1 - 6"<<endl;
+        }
+    }
+    return 0;
+}
+
+
 void question_1() {
     int rain_1, rain_2, rain_3; 
     string month_1, month_2, month_3;
@@ -87,7 +132,69 @@ void question_3(){
     }
 }
 
+void question_4(){
+    int area_choice;
+    cout << "Geometry Calculator"<< endl;
+    cout << "1. Calculate the Area of a Circle" << endl;
+    cout << "2. Calculate the Area of a Rectangle" << endl;
+    cout << "3. Calculate the Area of a Triangle" << endl;
+    cout << "4. Quit" << endl;
+    cout << "Enter your choice (1-4): ";
+    cin >> area_choice;
+    if (area_choice == 1){
+        double radius;
+        cout << "Enter radius of circle: ";
+        cin >> radius;
+        if (radius > 0){
+            cout << "The area of the circle is " << 3.14159*(radius*radius)<<endl;
+        }
+        else{
+            cout << "Radius has to be more than 0"<< endl;
+        }
+    }
+    else if(area_choice == 2){
+        double length, width;
+        cout << "What is length: ";
+        cin >> length;
+        cout << "What is width: ";
+        cin >> width;
+        if (length > 0 || width > 0){
+            cout << "Area of Rectangle: "<< length*width <<endl;
+        }
+        else{
+            cout << "Enter greater values"<< endl;
+        }
+    }
+    else if (area_choice == 3){
+        double base, height
+        cout << "What is base: ";
+        cin >> base;
+        cout << "What is height: ";
+        cin >> height;
+        if (base > 0 || height > 0){
+            cout << "Area of Rectangle: "<< base*height *0.5 <<endl;
+        }
+        else{
+            cout << "Enter greater values" << endl;
+        }
+    }
+    else if (area_choice == 4){
+        cout << "Returning to menu" << endl;
+    }
+    else{
+        cout << "Choices are 1-4" << endl;
+    }
 
-void question_4();
-void question_5();
-void question_6();
+
+
+}
+
+void question_5(){
+    double speed;
+    int time;
+    cout << "What is the speed of the vehicle in MPH? ";
+    cin >> speed;
+    cout << "How many hours has it traveled? ";
+    cin >> time;
+
+}
